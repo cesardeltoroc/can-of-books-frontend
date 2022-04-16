8304cb86f705b8d9d432e184126e54286c6e7124
laptopadventure <bazelart13@gmail.com>
2022-04-15 19:01:42 -0700

    about page fix

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ class Profile extends Component {
   render() {
     /* TODO: render information about logged in user */
     /* STRETCH TODO: if no logged in user then redirect home */
-    return <p>Profile page coming soon</p>
+    return <p>About page coming soon!</p>
   }
 };
 
